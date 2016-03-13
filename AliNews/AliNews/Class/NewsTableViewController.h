f4912b470cf250fb59e816a353e4192c16f66d1d
HuangLc <3283180700@qq.com>
2016-03-13 23:10:14 +0800

    添加工具类，和添加AFN 但是AFN 显示太慢 不能commit

--- AliNews/AliNews/Class/NewsTableViewController.h
+++ AliNews/AliNews/Class/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  AliNews
 //
 //  Created by 黄礼潮 on 16/3/13.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
