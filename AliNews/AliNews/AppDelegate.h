c71794568fd5dd3e7aa28b34a2bf72b0b42e57f4
HuangLc <3283180700@qq.com>
2016-03-13 22:53:55 +0800

    使用git

---
+++ AliNews/AliNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliNews
+//
+//  Created by 黄礼潮 on 16/3/13.
+//  Copyright © 2016年 黄礼潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AliNews/AliNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliNews
+//
+//  Created by 黄礼潮 on 16/3/13.
+//  Copyright © 2016年 黄礼潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
